40cb00fd2e12e11563fb83dd4fc4982ed54087c0
Matias Lenci <matiaslenci@gmail.com>
2026-03-09 23:32:02 -0300

    remove dist

--- dist/agencies/dto/update-agency.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class UpdateAgencyDto {
-    username?: string;
-    email?: string;
-    nombre?: string;
-    logo?: string;
-    portada?: string;
-    ubicacion?: string;
-    whatsapp?: string;
-}
--- dist/auth/dto/create-agency.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { Plan } from 'src/database/agency.entity';
-export declare class CreateAgencyDto {
-    plan?: Plan;
-    nombre: string;
-    username: string;
-    email: string;
-    password: string;
-}
